1bd4486d1836462e140e0cb4390db54d906ac870
Anastasia Pulina <yanssu@mail.ru>
2024-07-22 22:28:58 +0300

    Added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int[] myArray = {1, 2, 5, 0, 10, 34};
+
+// Решение задачи
+
+void ConvertArray(int[] array, int i)
+{
+    if (i >= 0)
+    {
+            Console.Write(array[i] + " ");
+            ConvertArray(array, i - 1);
+    }
+}
+ConvertArray(myArray, myArray.Length - 1);
